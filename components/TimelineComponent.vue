1785237b518d48c20a1ddd3538665a9218df9510
fotiemconstant <fotiemoghommahieconstant@gmail.com>
2021-09-10 22:22:31 +0100

    Added the timeline section to the portfolio still have to fill in the information on to it tho...

---
+++ components/TimelineComponent.vue
@@ -0,0 +1,142 @@
+<template>
+  <div class="container mt-16">
+    <div class="text-2xl text-center">
+      My story
+    </div>
+    <div class="text-center text-gray-600 text-sm mt-3">
+      Here is a little of my story.I'm a passionate of technology and fell in
+      love with the beauty of code. Below is a brief story of mine.
+    </div>
+    <div class="p-4 md:p-10">
+      <div class="md:w-2/3 mx-auto">
+        <div class="relative mt-5 text-left">
+          <div class="flex items-center relative">
+            <div class="hidden md:block w-20">
+              <div class="font-bold italic">
+                2020
+              </div>
+              <div class="md:flex space-x-1 text-xs">
+                <div>Abr.</div>
+                <div>-</div>
+                <div>Jun.</div>
+              </div>
+            </div>
+            <div
+              class="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-1"
+            >
+              <i class="fas fa-circle -top-1 -ml-2 absolute" />
+            </div>
+
+            <div class="ml-10">
+              <div class="font-bold">
+                Proyecto 1
+              </div>
+              <div class="italic md:mb-4">
+                Empresa 1
+              </div>
+              <div class="mb-4 mt-2 md:hidden">
+                <div class="font-bold">
+                  2020
+                </div>
+                <div class="text-xs">
+                  Abril - Junio
+                </div>
+              </div>
+              <div class="mb-10">
+                This year marked the beginning of everything, did my first steps
+                into coding. started here with Pascal and HTML, back then i was
+                soo much excited about building something on my own. It was soo
+                exciting.
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-center relative">
+            <div class="hidden md:block w-20">
+              <div class="font-bold italic">
+                2020
+              </div>
+              <div class="md:flex space-x-1 text-xs">
+                <div>Jul.</div>
+                <div>-</div>
+                <div>Ago.</div>
+              </div>
+            </div>
+
+            <div
+              class="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-1"
+            >
+              <i class="fas fa-circle -top-1 -ml-2 absolute" />
+            </div>
+
+            <div class="ml-10">
+              <div class="font-bold">
+                Proyecto 2
+              </div>
+              <div class="italic md:mb-4">
+                Empresa 2
+              </div>
+              <div class="mb-4 mt-2 md:hidden">
+                <div class="font-bold">
+                  2020
+                </div>
+                <div class="text-xs">
+                  Julio - Agosto
+                </div>
+              </div>
+              <div class="mb-10">
+                This year marked the beginning of everything, did my first steps
+                into coding. started here with Pascal and HTML, back then i was
+                soo much excited about building something on my own. It was soo
+                exciting.
+              </div>
+            </div>
+          </div>
+
+          <div class="flex items-center relative">
+            <div class="hidden md:block w-20">
+              <div class="font-bold italic">
+                2020
+              </div>
+              <div class="md:flex space-x-1 text-xs">
+                <div>Ago.</div>
+                <div>-</div>
+                <div>Hoy.</div>
+              </div>
+            </div>
+
+            <div
+              class="border-r-2 border-black absolute h-full left-1 md:left-20 top-2 z-1"
+            >
+              <i class="fas fa-circle -top-1 -ml-2 absolute" />
+              <i class="fas fa-circle bottom-0 -ml-2 absolute" />
+            </div>
+
+            <div class="ml-10">
+              <div class="font-bold">
+                Started code
+              </div>
+              <div class="italic md:mb-4">
+                Empresa 3
+              </div>
+              <div class="mb-4 mt-2 md:hidden">
+                <div class="font-bold">
+                  2012
+                </div>
+                <div class="text-xs">
+                  Ago. - Hoy.
+                </div>
+              </div>
+              <div class="mb-10">
+                This year marked the beginning of everything, did my first steps
+                into coding. started here with Pascal and HTML, back then i was
+                soo much excited about building something on my own. It was soo
+                exciting.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -13,11 +13,31 @@
       <div>
         <WhatProplesayAboutMe />
       </div>
+      <div>
+        <TimelineComponent />
+      </div>
     </div>
     <!-- <FooterSection /> -->
   </div>
 </template>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/js/all.min.js"></script>
 
 <script>
 export default {}
 </script>
+<style>
+.seccion {
+  text-transform: uppercase;
+  -webkit-transform: rotate(270deg);
+  -moz-transform: rotate(270deg);
+  -ms-transform: rotate(270deg);
+  -o-transform: rotate(270deg);
+  transform: rotate(270deg);
+  -webkit-transform-origin: 0 0;
+  -moz-transform-origin: 0 0;
+  -ms-transform-origin: 0 0;
+  -o-transform-origin: 0 0;
+  transform-origin: 0 0;
+  top: 60%;
+}
+</style>
